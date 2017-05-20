3b7e5c93d3da4cf77c76d88ec2359d202b60cd70
JYP in MBP 15 <me@ebadaq.com>
2017-05-20 14:34:53 +0900

    CinemaKid Clients

---
+++ CinemaKid/CinemaKid/CinemaModel.swift
@@ -0,0 +1,63 @@
+//
+//  CinemaModel.swift
+//  CinemaKid
+//
+//  Created by Abraham Park on 11/29/16.
+//  Copyright © 2016 ebadaq.com. All rights reserved.
+//
+import UIKit
+import Foundation
+
+class CinemaModel {
+    var arrayResult:Array<AnyObject> = []
+    var closureUpdateUI:(() -> ())?
+    
+    func requestToServer(){
+        let stringURL = "http://z.ebadaq.com:45070/CinemaKid/movie/list/"
+        let url = URL(string: stringURL)!
+        let request = URLRequest(url: url)
+        
+        let session = URLSession(configuration: URLSessionConfiguration.ephemeral)
+        
+        let task = session.dataTask(with: request,
+                                    completionHandler: {(data:Data?, resp:URLResponse?, err:Error?) -> Void
+            in
+            print(String(data: data!, encoding:String.Encoding.utf8)!)
+            do{
+               let result = try JSONSerialization.jsonObject(with: data!,
+                                                    options: .allowFragments)
+                var temp = result as! Dictionary<String,AnyObject>
+                
+                self.arrayResult = temp["data"] as! Array<AnyObject>
+                
+                if let updateUI = self.closureUpdateUI {
+                    OperationQueue.main.addOperation({ Void -> Void in
+                        updateUI()
+                    })
+                    
+                }
+                
+//                (((UIApplication.shared.delegate as! AppDelegate).window!.rootViewController as! UINavigationController).viewControllers.last as! MainTableViewController).tableView.reloadData()
+                
+            }catch{}
+    })
+        task.resume()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ CinemaKid/CinemaKid/MainTableViewController.swift
@@ -0,0 +1,73 @@
+//
+//  MainTableViewController.swift
+//  CinemaKid
+//
+//  Created by Abraham Park on 11/29/16.
+//  Copyright © 2016 ebadaq.com. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewController : UITableViewController {
+    let queueSub = OperationQueue()
+    var modelCinema = CinemaModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.queueSub.maxConcurrentOperationCount = 1
+        
+        self.modelCinema.requestToServer()
+        self.modelCinema.closureUpdateUI = { [unowned self] () -> () in
+            self.tableView.reloadData()
+        }
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.modelCinema.arrayResult.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        
+        var dicTemp = self.modelCinema.arrayResult[indexPath.row]
+        var dicInfo = dicTemp as! Dictionary<String,String>
+        
+        cell.textLabel?.text = dicInfo["title"]
+        cell.detailTextLabel?.text
+            = "\(dicInfo["genre"]), \(dicInfo["runningTime"]) 분"
+        
+        cell.imageView?.image = UIImage(named: "loading.png")
+        
+        let stringURL = "http://z.ebadaq.com:45070/CinemaKid/movie/stillcut/\(dicInfo["posterCode"]!)"
+        let url = URL(string: stringURL)!
+        self.queueSub.addOperation({ Void -> Void in
+            do {
+            let data = try Data(contentsOf: url)
+            
+                OperationQueue.main.addOperation({ 
+                    cell.imageView?.image = UIImage.init(data: data)
+                })
+            } catch {}
+            })
+        
+        
+        return cell
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
